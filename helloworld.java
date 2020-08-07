e21035306dccc051a55607664830d4ffb98e0fcc
Radman Siddiki <61620631+R4356th@users.noreply.github.com>
2020-08-07 14:51:59 +0600

    Edit Java code

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
-public class Main
+class Hello
 {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+    public static void main(String [] args)
+    {
+        System.out.println("hello, world");
+    }
 }
 
