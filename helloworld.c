c1edc2a862686f6d1572775a7d912758e9ac771e
Fatal <prathyaym@gmail.com>
2026-01-02 12:20:55 +0530

    Add return statement to main function

--- helloworld.c
+++ helloworld.c
@@ -3,4 +3,5 @@
 int main() //Declaring the main function of the program
 {
     printf("Hello, world!"); //Using the printf function to print "Hello, world!"
+    return 0; // new! returnng 0 doesnt accomplish anything but is still worshiped in c
 }
